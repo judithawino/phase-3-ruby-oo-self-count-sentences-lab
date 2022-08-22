0e2d831c6a98a661f59a507bf10853782245256a
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-08-22 07:59:11 +0300

    commit tree a0ebf52b126682260f479d688bbc71c46aa47534

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    "#{self}".end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    
+    # /\.|\?|!/ is a regular expression that searches for periods, question marks and exclamations in a string
+    # binding.pry
+    self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size 
   end
 end
+
+# "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
